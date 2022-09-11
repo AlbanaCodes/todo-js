dda2f25c580441614a62f9e1539fd94fa7f5c05c
Albana <albane.04@protonmail.com>
2022-09-11 22:36:43 +0200

    search function + small improvements

--- src/app.js
+++ src/app.js
@@ -1,7 +1,8 @@
-const searchForm = document.querySelector('.search input');
+const searchFormInput = document.querySelector('.search input');
 const addForm = document.querySelector('.add');
 const todoList = document.querySelector('.todos');
 const emptyListText = document.querySelector('.emptyTodo');
+const searchForm = document.querySelector('.search');
 
 console.log(todoList);
 
@@ -27,6 +28,8 @@ const generateTodoTemplate = (todo) => {
 };
 
 addForm.addEventListener('submit', e => {
+  console.log('submit1');
+  console.log('addForm: ', addForm);
   e.preventDefault();
   const todo = addForm.add.value.trim(); // removes leading and trailing empty spaces
   if(todo.length){
@@ -42,4 +45,40 @@ todoList.addEventListener('click', e => {
     e.target.parentElement.remove(); // e.target is the <i> and parentElement is the <li> in which the <i> is inside
     checkTodoListLength();
   }
+});
+
+// search and filter todos
+const filterTodos = (searchTerm) => {
+  // hide todos that don't match with the term
+  Array.from(todoList.children)
+    .filter((todo) => {
+      // return the todos that don't match, to add the "hide" class in the forEach
+      return !todo.textContent.toLowerCase().includes(searchTerm);
+    })
+    .forEach((todo) => {
+      todo.classList.add('hideElement');
+    });
+
+  // find the todos that match with the search term and remove the "hide" class, so that it becomes visible
+  Array.from(todoList.children)
+    .filter((todo) => {
+      return todo.textContent.toLowerCase().includes(searchTerm);
+    })
+    .forEach((todo) => {
+      todo.classList.remove('hideElement');
+    })
+};
+
+searchForm.addEventListener('submit', e => {
+  e.preventDefault();
+});
+
+searchForm.addEventListener('focusout', e => {
+  Array.from(todoList.children).forEach((todo) => todo.classList.remove('hideElement'));
+  searchForm.reset();
+});
+
+searchFormInput.addEventListener('keyup', (e) => {
+  const searchTerm = searchFormInput.value.trim().toLowerCase();
+  filterTodos(searchTerm);
 });
